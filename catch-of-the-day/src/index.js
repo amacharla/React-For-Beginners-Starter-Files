674a160acb58735d73e7db181c59dc55ffeff978
Anoop Macharla <anoopm@MacBook-Pro-3.local>
2017-11-28 23:28:24 -0800

    day 1 done

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom'; // only imports one method
+import { BrowserRouter, Match, Miss } from 'react-router';
+import StorePicker from './components/StorePicker';
+import App from './components/App';
+import NotFound from './components/NotFound';
+import './css/style.css';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.getElementById('main'));

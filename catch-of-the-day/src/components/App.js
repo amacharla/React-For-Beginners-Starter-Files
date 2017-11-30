3ba94d4b4cf2d662c078d091744f06f5ed581262
Anoop Macharla <anoopm@MacBook-Pro-3.local>
2017-11-30 01:03:43 -0800

    Day 2 Learning States

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,26 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.addFish = this.addfish.bind(this); // addFish can access
+    // get initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    //update our state
+    const fishes = {...this.state.fishes}; //unpacks and makes a copy of fishes object
+    //add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-$[timestamp]`] = fish; // set key to be fish-TIMESTAMP & value to new fish obj
+    //set state
+    this.setState({ fishes }); // same as {fishes: fishe}
+  }
+
   render() {
     return (
     <div className="catch-of-the-day">
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,16 +2,28 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+  gotToStore(event) {
+    event.preventDefault(); // prevent the default action of js -refreshing
+    const storeId = this.storeInput.value; //getting value from input
+    this.context.router.transitionTo(`/store/${storeId}`); //transitiong and setting the uid as value
+    // pay attention to ` above, its not ' !!!
+  }
+
   render() {
     return (
-      <form className="store-selector"> {/*can only have one parent*/}
+      <form className="store-selector" onSubmit={this.gotToStore.bind(this)}>
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name"
-          defaultValue={getFunName()}/>
+          defaultValue={getFunName()} ref={(input) => { this.storeInput = input }}/>
         <button type="submit">Visit Store -></button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {  //access the router object
+  router: React.PropTypes.object
 }
 
 export default StorePicker;

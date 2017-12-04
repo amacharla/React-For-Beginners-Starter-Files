10a55bace055f11afb1130e010da5ec74f134cb2
Anoop Macharla <anoopm@MacBook-Pro-3.local>
2017-12-03 17:58:33 -0800

    Order configuration setup and connected to Firebase!

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  // taken from FIREBASE
+  apiKey: "AIzaSyDbNfDG917Q5FiixEGMbX7ZpmSOWvCr3qc",
+  authDomain: "catch-of-the-day-nooplion.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-nooplion.firebaseio.com"
+});
+
+export default base;
